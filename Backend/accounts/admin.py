e1f70311a6cbe24bb17ce50b83d143be8e12d290
anthonynarine <anarine83@gmail.com>
2023-07-05 13:17:43 -0400

    Project restarted - inital commit

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
---
+++ Backend/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    name = models.CharField(null=True, blank=True, max_length=100) 
+
+    def __str__(self):
+        return self.name
