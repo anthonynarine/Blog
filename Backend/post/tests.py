4cec6a199f34d873f9bbc72470b8d7f0aee37a1d
anthonynarine <anarine83@gmail.com>
2023-07-13 16:57:36 -0400

    custom user and post model test completed

--- Backend/post/tests.py
+++ Backend/post/tests.py
@@ -1,2 +1,34 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import testcases
+
+from .models import Post
+
+# command python3 manage.py test
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+        username = "batman", 
+        email = "test@email.com",
+        password = "batcave",
+    )
+
+        cls.post = Post.objects.create(
+            author = cls.user,
+            title = "batman begins",
+            body = "why so serious"
+        )
+    
+    def test_post_model(self):
+        self.assertEqual(self.post.author.username, "batman")
+        self.assertEqual(self.post.title, "batman begins")
+        self.assertEqual(self.post.body, "why so serious")
+        self.assertEquals(str(self.post), "batman begins")
+
+
+
+        
+
+
