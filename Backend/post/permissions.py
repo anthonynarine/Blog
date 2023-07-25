5b019e7818a4d61673a1f5198301af5388bf52b0
anthonynarine <anarine83@gmail.com>
2023-07-25 13:33:54 -0400

    tested permission class: user allowed to view post list and able to delete authored post from detail view

--- Backend/post/permissions.py
+++ Backend/post/permissions.py
@@ -1,18 +1,44 @@
 from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        """Requires that a user be logged in or authenticated in order to have access"""
-        if request.user.is_authenticated:
-            return True
-        else:
-            return False
+    """
+    Custom permission to only allow authors of a post to edit it.
+    Other authenticated users only have read permissions.
+    """
 
     def has_permission(self, request, view):
-        # Read Permissions are allowed to any request we we'll always 
-        # allow GET, HEAD, or OPTIONS requests
+        """
+        Checks whether the user is authenticated, allowing access 
+        to any authenticated user 
+        
+        Parameters:
+        request (rest_framework.request.Request): Standard DRF request object.
+        view (rest_framework.views.View): Standard DRF view object.
+
+        Returns:
+        bool: True if the user is authenticated, else False
+        """
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Object-level permission check.
+
+        Allow GET, HEAD, or OPTIONS requests for any user (authenticated or not).
+        Only allow edits (PUT, PATCH, DELETE) if the post is authored by the
+        user making the request.
+
+        Parameters:
+        request (rest_framework.request.Request): Standard DRF request object.
+        view (rest_framework.views.View): Standard DRF view object.
+        obj (Post): The post object that is being accessed.
+
+        Returns:
+        bool: True if permission is granted, else False
+        """
+        # Read permissions are allowed for any request
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissisons are only allowed to the author of a post
-        return object.author == request.user
+        # Write permissions are only allowed to the author of a post
+        return obj.author == request.user
--- Backend/post/views.py
+++ Backend/post/views.py
@@ -5,12 +5,11 @@ from .models import Post
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (IsAuthorOrReadOnly)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all().order_by("created_at")
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
