d898e60689616e7a208df76c4e40dcca36e95a73
anthonynarine <anarine83@gmail.com>
2023-07-25 11:22:26 -0400

    permissions added allowing only logged in users to have access + if the user is not the author they have read only permission, only authors have write permissions

--- Backend/post/permissions.py
+++ Backend/post/permissions.py
@@ -1,4 +1,18 @@
 from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    
+    def has_permission(self, request, view):
+        """Requires that a user be logged in or authenticated in order to have access"""
+        if request.user.is_authenticated:
+            return True
+        else:
+            return False
+
+    def has_permission(self, request, view):
+        # Read Permissions are allowed to any request we we'll always 
+        # allow GET, HEAD, or OPTIONS requests
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissisons are only allowed to the author of a post
+        return object.author == request.user
