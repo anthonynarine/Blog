b44638d6e3da9dbc519905963f93d732bb1cbcf7
anthonynarine <anarine83@gmail.com>
2023-07-25 13:06:54 -0400

    permissions updated on PostList and PostDetail view with new custom permission classes

--- Backend/post/views.py
+++ Backend/post/views.py
@@ -1,19 +1,20 @@
 from rest_framework import generics, permissions
+from .permissions import IsAuthorOrReadOnly
 
 from .models import Post
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
+    pagination_class = (IsAuthorOrReadOnly)
     queryset = Post.objects.all().order_by("created_at")
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    pagination_class = (IsAuthorOrReadOnly)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-# this permission class will not allow logged out users to view post
-# logged in users can view the list page, but only admin users can see the  detial page
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    # permission_classes = (permissions.IsAdminUser)
+    pagination_class = (IsAuthorOrReadOnly)
     querysetm = Post.objects.all()
     serializer_class = PostSerializer
